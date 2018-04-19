2fb65eea9ce5cc9350eae7ffd7b218aea2ae349f
Andrea RIZZELLO <arizzell@e1r1p8.42.fr>
2018-04-19 19:20:18 +0200

    TODO verifica utente

--- backend/functions.php
+++ backend/functions.php
@@ -72,6 +72,9 @@ if (count($_POST) === 5 && isset($_POST['username'], $_POST['email'], $_POST['pa
     }
     if (empty($username) || empty($password) || empty($name) || empty($surname))
         responseCode(400, -1);
+    $hash = hash("md5", $username.$password);
+    $name[0] = strtoupper($name[0]);
+    $surname[0] = strtoupper($surname[0]);
     if(!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL))
         responseCode(400, -1);
     $email = $_POST['email'];
@@ -87,17 +90,21 @@ if (count($_POST) === 5 && isset($_POST['username'], $_POST['email'], $_POST['pa
     else
     {
         try {
-            $query = "INSERT INTO `users` (`username`, `email`, `password`, `name`, `surname`) VALUES (?, ?, ?, ?, ?)";
+            $query = "INSERT INTO `users` (`username`, `email`, `password`, `name`, `surname`, `hash`) VALUES (?, ?, ?, ?, ?, ?)";
             $db = $pdobj->prepare($query);
             $db->bindParam(1, $username, PDO::PARAM_STR);
             $db->bindParam(2, $email, PDO::PARAM_STR);
             $db->bindParam(3, $password, PDO::PARAM_STR);
             $db->bindParam(4, $name, PDO::PARAM_STR);
             $db->bindParam(5, $surname, PDO::PARAM_STR);
+            $db->bindParam(6, $hash, PDO::PARAM_STR);
             if ($db->execute())
             {
-                //TODO:ADDARE IL MESSAGGIO CON IL CODICE
-                mail($email,"[ACCOUNT VERIFICATION]", "This is the code", "From: <arizzell@student.42.fr>\r\n");
+                $fullname = $name." ".$surname;
+                $header = 'Content-type: text/html; charset=UTF-8' . "\r\n";
+                $header .= 'From: <arizzell@student.42.fr>' . "\r\n";
+                $msg = 'Hello '.$fullname.'<br>To finalyze your subscribtion please click <a href="http://'.$_SERVER['HTTP_HOST'].'/verify.php?token='.$hash.'">HERE</a> to validate your profile';
+                mail($email,"[ACCOUNT VERIFICATION]", $msg, $header);
                 responseCode(201, 0);
             }
             else
@@ -118,4 +125,31 @@ if (count($_POST) === 1 && isset($_POST['destroy']))
 {
     session_destroy();
     responseCode(200, 0);
+}
+
+//VERIFY ACCOUNT
+if (count($_GET) === 1 && isset($_GET['token']))
+{
+    $hash = preg_replace("/[^a-fA-F0-9]/", "", trim($_GET['token']));
+    try {
+        $pdobj = new PDO($DB_DNS, $DB_USER, $DB_PASSWORD);
+        $pdobj->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $pdobj->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+    } catch (PDOException $e) {
+        die('Connection failed: ' . $e->getMessage());
+    }
+    $query = "SELECT iduser FROM `users` WHERE `hash` = ?";
+    $db = $pdobj->prepare($query);
+    $db->bindParam(1,$hash);
+    $db->execute();
+    $result = $db->fetch(PDO::FETCH_ASSOC);
+    if ($result)
+    {
+        $query = "UPDATE `users` SET `activated` = 1 WHERE iduser = ?";
+        $db->bindParam(1,$result['iduser'], PDO::PARAM_INT);
+        $db->execute();
+        responseCode(200, 0);
+    }
+    else
+        responseCode(304, -1);
 }
---
+++ verify.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0">
+    <title>Camagru - Verify</title>
+    <link rel="icon" type="image/png" sizes="32x32" href="imgs/favicon.png">
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="css/body.css">
+    <link href="https://fonts.googleapis.com/css?family=Pacifico|Roboto:300" rel="stylesheet">
+</head>
+<body>
+<div style="text-align: center; width: 100%">
+    <img src="imgs/thumbs_up.gif">
+    <h1 style="font-family: 'Roboto', sans-serif; font-size: 500%">Awesome!</h1>
+    <p style="font-family: 'Roboto', sans-serif">Your account has been verified, you can now login</p>
+    <a href="index.php"><button class="btn-green"><span class="fix-skew-greenbtn">Go back!</span></button></a>
+<!--    TODO:Da finire la verifica dell'utente-->
+</div>
+</body>
+</html>
